8b0b37b0e572bd42ab66ca68dfcdca83369a07e0
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-09-26 20:48:10 -0400

    License.java

--- License.java
+++ License.java
@@ -1,5 +1,32 @@
 package SER315_GroupProject;
 
 public class License {
-
+	
+	//License attributes
+	String purchaseDate;
+	String expiryDate;
+	String ownerName;
+	CAT currentCAT;
+	
+	//License constructor
+	public License(String purchaseDate, String expiryDate, String ownerName, CAT currentCAT) {
+		this.purchaseDate = purchaseDate;
+		this.expiryDate = expiryDate;
+		this.ownerName = ownerName;
+		this.currentCAT = currentCAT;
+	}
+	
+	//Getters and setters
+	public String getPurchaseDate() {
+		return purchaseDate;
+	}
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public CAT getCurrentCAT() {
+		return currentCAT;
+	}
 }
