c70521aeba4fa8bf25084209dcca7b720799efd0
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-09-26 20:43:25 -0400

    Racers.java

--- Racers.java
+++ Racers.java
@@ -1,5 +1,47 @@
 package SER315_GroupProject;
 
-public class Racers {
+import java.util.ArrayList;
 
+public class Racers {
+	
+	//Racers attributes
+	int racerID;
+	String name;
+	CAT currentCAT;
+	ContactInfo contactInfo;
+	ArrayList<Race> upcomingRaces;
+	ArrayList<Race> completedRaces;
+	License license;
+	
+	//Racers constructor
+	public Racers(int racerID, String name, CAT currentCAT, ContactInfo contactInfo, 
+			ArrayList<Race> upcomingRaces, ArrayList<Race> completedRaces, License license) {
+		this.racerID = racerID;
+		this.name = name;
+		this.currentCAT = currentCAT;
+		this.contactInfo = contactInfo;
+		this.upcomingRaces = upcomingRaces;
+		this.completedRaces = completedRaces;
+		this.license = license;
+	}
+	
+	//Racers can sign up for a new race
+	public void signUpForRace(int raceID) {
+		
+	}
+	
+	//Racers can review a race they have completed
+	public void reviewRace(int raceID) {
+		
+	}
+	
+	//Returns completed races arraylist
+	public ArrayList<Race> viewRaceHistory() {
+		return completedRaces;
+	}
+	
+	//Racers can purchase a new license
+	public void purchaseLicense() {
+		
+	}
 }
