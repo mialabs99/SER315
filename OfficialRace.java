e82dd524254abc6452deae52ca9f4d06575065be
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-09-26 20:27:56 -0400

    OfficialRace.java

--- OfficialRace.java
+++ OfficialRace.java
@@ -1,5 +1,23 @@
 package SER315_GroupProject;
 
-public class OfficialRace {
+import java.util.ArrayList;
 
+public class OfficialRace {
+	
+	//ArrayList that holds the results of each official race
+	ArrayList<RaceResults> results;
+	
+	//OfficialRace constructor
+	public OfficialRace(ArrayList<RaceResults> results) {
+		this.results = results;
+	}
+	
+	//Add new results to an official race
+	public void addResults(RaceResults newResults) {
+		results.add(newResults);
+	}
+	
+	public RaceResults getOfficialRaceResults(int raceID) {
+		return results.get(raceID);
+	}
 }
