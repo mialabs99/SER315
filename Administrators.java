7cde406f1d08aa06a1444e7c88e44478efc73c11
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-09-26 20:05:29 -0400

    Administrators.java

--- Administrators.java
+++ Administrators.java
@@ -1,4 +1,46 @@
 package SER315_GroupProject;
-public class Administrators {
 
+public class Administrators {
+	
+	//Administrator attributes
+	private int adminID;
+	private String name;
+	
+	//Administrator constructor
+	public Administrators(int adminID, String name){
+		this.adminID = adminID;
+		this.name = name;
+	}
+	
+	//Selects a user account to manage
+	public void ManageUserAccounts() {
+		
+	}
+	
+	//Selects a license to manage
+	public void ManageLicenses() {
+		
+	}
+	
+	//Selects a setting to manage
+	public void ManageSystemSettings() {
+		
+	}
+	
+	public int getAdminID() {
+		return adminID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setAdminID(int newID) {
+		adminID = newID;
+	}
+	
+	public void setName(String newName) {
+		name = newName;
+	}
 }
+
