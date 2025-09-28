621c213675be01e84def830fecff1c6b14941da1
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-09-28 14:25:48 -0400

    RaceResults.java

--- RaceResults.java
+++ RaceResults.java
@@ -1,5 +1,27 @@
 package SER315_GroupProject;
 
-public class RaceResults {
+import java.util.ArrayList;
 
+public class RaceResults {
+	
+	//RaceResults attributes
+	private ArrayList<Racers> raceResults;
+	
+	//RaceResults constructor
+	public RaceResults(ArrayList<Racers> raceResults) {
+		this.raceResults = raceResults;
+	}
+	
+	//Adds results of a completed race to a racer's profile
+	public void addRaceToProfile() {
+		
+	}
+	
+	//Getter and setter
+	public ArrayList<Racers> getRaceResults() {
+		return raceResults;
+	}
+	public void setRaceResults(ArrayList<Racers> raceResults) {
+		this.raceResults = raceResults;
+	}
 }
