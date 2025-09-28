7a6e9f9b7b0c8ab10f71bf1b56ee98176be9997f
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-09-28 13:47:17 -0400

    ContactInfo.java

---
+++ ContactInfo.java
@@ -0,0 +1,37 @@
+package SER315_GroupProject;
+
+//Class that holds user's contact info
+public class ContactInfo {
+		
+		//Contact info attributes
+		private String phoneNumber;
+		private String address;
+		private String email;
+		
+		//Contact info constructor
+		public ContactInfo(String phoneNumber, String address, String email) {
+			this.phoneNumber = phoneNumber;
+			this.address = address;
+			this.email = email;
+		}
+		
+		//Getters and setters
+		public String getPhoneNumber() {
+			return phoneNumber;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setPhoneNumber(String phoneNumber) {
+			this.phoneNumber = phoneNumber;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+}
