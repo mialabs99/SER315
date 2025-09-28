c908d64126b13f3d833ad44c765d42ded873b1db
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-09-28 13:40:02 -0400

    Organizers.java

--- Organizers.java
+++ Organizers.java
@@ -3,45 +3,9 @@ package SER315_GroupProject;
 public class Organizers {
 
 	//Organizer attributes
-	int organizerID;
-	String name;
-	ContactInfo contactInfo;
-	
-	//Class that holds the organizer's contact info
-	public class ContactInfo{
-		
-		//Contact info attributes
-		String phoneNumber;
-		String address;
-		String email;
-		
-		//Contact info constructor
-		public ContactInfo(String phoneNumber, String address, String email) {
-			this.phoneNumber = phoneNumber;
-			this.address = address;
-			this.email = email;
-		}
-		
-		//Getters and setters
-		public String getPhoneNumber() {
-			return phoneNumber;
-		}
-		public String getAddress() {
-			return address;
-		}
-		public String getEmail() {
-			return email;
-		}
-		public void setPhoneNumber(String phoneNumber) {
-			this.phoneNumber = phoneNumber;
-		}
-		public void setAddress(String address) {
-			this.address = address;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
-	}
+	private int organizerID;
+	private String name;
+	private ContactInfo contactInfo;
 	
 	//Organizers constructor
 	public Organizers(int organizerID, String name, ContactInfo contactInfo) {
@@ -69,4 +33,26 @@ public class Organizers {
 	public void updateResults() {
 		
 	}
+	
+	//Getters and setters
+	public int getOrganizerID() {
+		return organizerID;
+	}
+	public String getName() {
+		return name;
+	}
+	public ContactInfo getContactInfo() {
+		return contactInfo;
+	}
+	public void setOrganizerID(int organizerID) {
+		this.organizerID = organizerID;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setContactInfo(ContactInfo contactInfo) {
+		this.contactInfo = contactInfo;
+	}
 }
+
