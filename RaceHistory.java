7dcc5eb040f39df5083681b05e9f277a199fcc22
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-09-28 14:09:16 -0400

    RaceHistory.java

--- RaceHistory.java
+++ RaceHistory.java
@@ -1,5 +1,22 @@
 package SER315_GroupProject;
 
-public class RaceHistory {
+import java.util.ArrayList;
 
+public class RaceHistory {
+	
+	//RaceHistory attribute
+	private ArrayList<Race> raceHistory;
+	
+	//RaceHistory constructor
+	public RaceHistory(ArrayList<Race> raceHistory) {
+		this.raceHistory = raceHistory;
+	}
+	
+	//Getter and setter
+	public ArrayList<Race> getRaceHistory() {
+		return raceHistory;
+	}
+	public void setRaceHistory(ArrayList<Race> raceHistory) {
+		this.raceHistory = raceHistory;
+	}
 }
