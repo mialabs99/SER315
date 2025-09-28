749e4672897f455810cf00de276fd538508ce68a
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-09-28 14:04:23 -0400

    CAT.java

--- CAT.java
+++ CAT.java
@@ -1,5 +1,35 @@
 package SER315_GroupProject;
 
 public class CAT {
-
+	
+	//Enumeration for the different categories
+	public enum Categories{
+		one, two, three, four, five
+	}
+	
+	//CAT attributes
+	private int levelUpStatus;
+	private Categories currentCAT;
+	
+	//CAT constructor
+	public CAT(int levelUpStatus, Categories currentCAT) {
+		this.levelUpStatus = levelUpStatus;
+		this.currentCAT = currentCAT;
+	}
+	
+	//Getters and setters
+	public int getLevelUpStatus() {
+		return levelUpStatus;
+	}
+	public Categories getCurrentCAT() {
+		return currentCAT;
+	}
+	public void setLevelUpStatus(int levelUpStatus) {
+		this.levelUpStatus = levelUpStatus;
+	}
+	public void setCurrentCAT(Categories currentCAT) {
+		this.currentCAT = currentCAT;
+	}
+	
 }
+
