e43ae47e3e27aa71d7723bed080f95799d6dac54
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-09-28 14:12:10 -0400

    UnofficialRace.java

--- UnofficialRace.java
+++ UnofficialRace.java
@@ -1,5 +1,24 @@
 package SER315_GroupProject;
 
-public class UnofficialRace {
+import java.util.ArrayList;
 
+public class UnofficialRace extends Race{
+	//ArrayList that holds the results of each official race
+	private ArrayList<RaceResults> results;
+	
+	//OfficialRace constructor
+	public UnofficialRace(String raceName, String raceDate, int numMiles, String raceRoute, ArrayList<String> stages,
+			Boolean isOfficial) {
+		super(raceName, raceDate, numMiles, raceRoute, stages, false);
+		this.results = new ArrayList<>();
+	}
+	
+	//Add new results to an official race
+	public void addResults(RaceResults newResults) {
+		results.add(newResults);
+	}
+	
+	public RaceResults getUnofficialRaceResults(int raceID) {
+		return results.get(raceID);
+	}
 }
