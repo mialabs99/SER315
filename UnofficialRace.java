9046fd77c18447bb617ae393684859adefdf369d
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-10-07 13:12:01 -0400

    Update UnofficialRace.java

--- UnofficialRace.java
+++ UnofficialRace.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 //Class that holds the information of the unofficial races
 public class UnofficialRace extends Race{
+
+    //ArrayList that holds all Unofficial Races
+    private ArrayList<Race> allUnofficialRaces;
+
 	//ArrayList that holds the results of each official race
 	private ArrayList<RaceResults> results;
 	
@@ -13,6 +17,16 @@ public class UnofficialRace extends Race{
 		super(raceName, raceDate, numMiles, raceRoute, stages, false);
 		this.results = new ArrayList<>();
 	}
+
+    //Adds an unofficial race to the ArrayList allUnofficialRaces
+    private void addUnofficialRace(Race unofficialRace){
+        allUnofficialRaces.add(unofficialRace);
+    }
+
+    //Removes an unofficial race from the ArrayList allUnofficialRaces
+    private void removeUnofficialRace(Race unofficialRace){
+        allUnofficialRaces.remove(unofficialRace);
+    }
 	
 	//Add new results to an official race
 	public void addResults(RaceResults newResults) {
@@ -22,5 +36,10 @@ public class UnofficialRace extends Race{
 	public RaceResults getUnofficialRaceResults(int raceID) {
 		return results.get(raceID);
 	}
+
+    //Getters and setters
+    public ArrayList<Race> getAllUnofficialRaces(){return allUnofficialRaces;}
+    public void setAllUnofficialRaces(ArrayList<Race> allUnofficialRaces){this.allUnofficialRaces = allUnofficialRaces;}
 }
+
 
