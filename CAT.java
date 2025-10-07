fc922e81eac5cba8eff893c64f4a8fdd1f6d7cc4
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-10-07 12:57:17 -0400

    CAT.java

--- CAT.java
+++ CAT.java
@@ -2,36 +2,37 @@ package SER315_GroupProject;
 
 //Class that holds the information for the racers' categories.
 public class CAT {
-	
-	//Enumeration for the different categories
-	public enum Categories{
-		one, two, three, four, five
-	}
-	
-	//CAT attributes
-	private int levelUpStatus;
-	private Categories currentCAT;
-	
-	//CAT constructor
-	public CAT(int levelUpStatus, Categories currentCAT) {
-		this.levelUpStatus = levelUpStatus;
-		this.currentCAT = currentCAT;
-	}
-	
-	//Getters and setters
-	public int getLevelUpStatus() {
-		return levelUpStatus;
-	}
-	public Categories getCurrentCAT() {
-		return currentCAT;
-	}
-	public void setLevelUpStatus(int levelUpStatus) {
-		this.levelUpStatus = levelUpStatus;
-	}
-	public void setCurrentCAT(Categories currentCAT) {
-		this.currentCAT = currentCAT;
-	}
-	
+
+    //Enumeration for the different categories
+    public enum Categories{
+        one, two, three, four, five
+    }
+
+    //CAT attributes
+    private int levelUpStatus;
+    private Categories currentCAT;
+
+    //CAT constructor
+    public CAT(int levelUpStatus, Categories currentCAT) {
+        this.levelUpStatus = levelUpStatus;
+        this.currentCAT = currentCAT;
+    }
+
+    //Getters and setters
+    public int getLevelUpStatus() {
+        return levelUpStatus;
+    }
+    public Categories getCurrentCAT() {
+        return currentCAT;
+    }
+    public void setLevelUpStatus(int levelUpStatus) {
+        this.levelUpStatus = levelUpStatus;
+    }
+    public void setCurrentCAT(Categories currentCAT) {
+        this.currentCAT = currentCAT;
+    }
+
 }
+
 
 
