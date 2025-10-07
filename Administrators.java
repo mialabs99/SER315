e9a001ff6a53204792e8f9bb319b7eed173ad980
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-10-07 12:56:39 -0400

    Administrators.java

--- Administrators.java
+++ Administrators.java
@@ -1,56 +1,56 @@
 package SER315_GroupProject;
 
+import java.util.ArrayList;
+
 //Class that holds administrator information.
-public class Administrators {
-	
-	//Administrator attributes
-	private int adminID;
-	private String name;
-	private ContactInfo contactInfo;
-	
-	//Administrator constructor
-	public Administrators(int adminID, String name, ContactInfo contactInfo){
-		this.adminID = adminID;
-		this.name = name;
-		this.contactInfo = contactInfo;
-	}
-	
-	//Selects a user account to manage
-	public void ManageUserAccounts() {
-		
-	}
-	
-	//Selects a license to manage
-	public void ManageLicenses() {
-		
-	}
-	
-	//Selects a setting to manage
-	public void ManageSystemSettings() {
-		
-	}
-	
-	//Getters and setters
-	public int getAdminID() {
-		return adminID;
-	}
-	public String getName() {
-		return name;
-	}
-	public ContactInfo getContactInfo() {
-		return contactInfo;
-	}
-	public void setAdminID(int adminID) {
-		this.adminID = adminID;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	public void setContactInfo(ContactInfo contactInfo) {
-		this.contactInfo = contactInfo;
-	}
+public class Administrators extends User {
+
+    //ArrayList holding all Administrators currently signed up for the program
+    private ArrayList<User> allAdministrators;
+
+    //Administrator attributes
+    private ContactInfo contactInfo;
+
+    //Administrator constructor
+    public Administrators(int adminID, String name, ContactInfo contactInfo){
+        super(adminID, name, Role.ADMIN);
+        this.contactInfo = contactInfo;
+    }
+
+    //Adds a new Administrator to the ArrayList allAdministrators
+    private void addAdministrator(User administrator){
+        allAdministrators.add(administrator);
+    }
+
+    //Removes an Administrator from the ArrayList allAdministrators
+    private void removeAdministrator(User administrator){
+        allAdministrators.remove(administrator);
+    }
+
+    //Selects a user account to manage
+    public void ManageUserAccounts(int id, String managementChoice) {
+
+    }
+
+    //Selects a license to manage
+    public void ManageLicenses() {
+
+    }
+
+    //Selects a setting to manage
+    public void ManageSystemSettings() {
+
+    }
+
+    //Getters and setters
+    public ContactInfo getContactInfo() {return contactInfo;}
+    public ArrayList<User> getAllAdministrators(){return allAdministrators;}
+    public void setContactInfo(ContactInfo contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+    public void setAllAdministrators(ArrayList<User> allAdministrators){this.allAdministrators = allAdministrators;}
 }
+
 
 
 
