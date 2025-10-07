fc5e4fa138811d475042d12707540df9bc564bbb
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-10-07 13:10:15 -0400

    Update Racers.java

--- Racers.java
+++ Racers.java
@@ -3,11 +3,12 @@ package SER315_GroupProject;
 import java.util.ArrayList;
 
 //Class that holds the racer's information.
-public class Racers {
-	
+public class Racers extends User{
+
+    //ArrayList holding all Racers currently signed up for the program
+    private ArrayList<User> allRacers;
+
 	//Racers attributes
-	private int racerID;
-	private String name;
 	private CAT currentCAT;
 	private ContactInfo contactInfo;
 	private ArrayList<Race> upcomingRaces;
@@ -17,18 +18,27 @@ public class Racers {
 	//Racers constructor
 	public Racers(int racerID, String name, CAT currentCAT, ContactInfo contactInfo, 
 			ArrayList<Race> upcomingRaces, ArrayList<Race> completedRaces, License license) {
-		this.racerID = racerID;
-		this.name = name;
+        super(racerID, name, Role.RACER);
 		this.currentCAT = currentCAT;
 		this.contactInfo = contactInfo;
 		this.upcomingRaces = upcomingRaces;
 		this.completedRaces = completedRaces;
 		this.license = license;
 	}
+
+    //Adds a new Racer to the allRacers ArrayList
+    private void addRacer(User racer){
+        allRacers.add(racer);
+    }
+
+    //Removes a Racer from the allRacers ArrayList
+    private void removeRacer(User racer){
+        allRacers.remove(racer);
+    }
 	
 	//Racers can sign up for a new race
-	public void signUpForRace(int raceID) {
-		
+	public void signUpForRace(RaceRegistration raceRegistration, int racerID) {
+
 	}
 	
 	//Racers can review a race they have completed
@@ -47,45 +57,19 @@ public class Racers {
 	}
 	
 	//Getters and setters
-	public int getRacerID() {
-		return racerID;
-	}
-	public String getName() {
-		return name;
-	}
-	public CAT getCurrentCAT() {
-		return currentCAT;
-	}
-	public ContactInfo getContactInfo() {
-		return contactInfo;
-	}
-	public ArrayList<Race> getUpcomingRaces() {
-		return upcomingRaces;
-	}
-	public ArrayList<Race> getCompletedRaces() {
-		return completedRaces;
-	}
-	public License getLicense() {
-		return license;
-	}
-	public void setRacerID(int racerID) {
-		this.racerID = racerID;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public void setCurrentCAT(CAT currentCAT) {
-		this.currentCAT = currentCAT;
-	}
-	public void setContactInfo(ContactInfo contactInfo) {
-		this.contactInfo = contactInfo;
-	}
-	public void setUpcomingRaces(ArrayList<Race> upcomingRaces) {
-		this.upcomingRaces = upcomingRaces;
-	}
-	public void setCompletedRaces(ArrayList<Race> completedRaces) {
-		this.completedRaces = completedRaces;
-	}
+	public CAT getCurrentCAT() {return currentCAT;}
+	public ContactInfo getContactInfo() {return contactInfo;}
+	public ArrayList<Race> getUpcomingRaces() {return upcomingRaces;}
+	public ArrayList<Race> getCompletedRaces() {return completedRaces;}
+	public License getLicense() {return license;}
+    public ArrayList<User> getAllRacers(){return allRacers;}
+	public void setCurrentCAT(CAT currentCAT) {this.currentCAT = currentCAT;}
+	public void setContactInfo(ContactInfo contactInfo) {this.contactInfo = contactInfo;}
+	public void setUpcomingRaces(ArrayList<Race> upcomingRaces) {this.upcomingRaces = upcomingRaces;}
+	public void setCompletedRaces(ArrayList<Race> completedRaces) {this.completedRaces = completedRaces;}
+    public void setAllRacers(ArrayList<User> allRacers){this.allRacers = allRacers;}
 }
+
+
 
 
