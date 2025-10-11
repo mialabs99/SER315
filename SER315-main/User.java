c4fc3fa7b0f9a95d536457f80633707e73277ed0
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-10-11 19:23:40 -0400

    Update User.java

--- SER315-main/User.java
+++ SER315-main/User.java
@@ -7,12 +7,24 @@ public class User implements IUser {
     private final int id;
     private String name;
     private Role role;
+    private ArrayList<User> allUsers;
 
     protected User(int id, String name, Role role) {
         this.id = id;
         this.name = name;
         this.role = role;
     }
+    
+    public User findUser(int userID){
+        for(int i = 0; i < allUsers.size(); i++){
+            if(allUsers.get(i).getId() == userID){
+                return allUsers.get(i);
+            }
+        }
+        System.out.println("Sorry, the user could not be found. Please try again.");
+        return null;
+    }
+    
     @Override public int getId()   {
         return id;
     }
