dffdf9623b43b55c91e3253a48342ae20cb3d80b
mialabs99 <126208040+mialabs99@users.noreply.github.com>
2025-10-11 19:53:31 -0400

    Update License.java

--- SER315-main/License.java
+++ SER315-main/License.java
@@ -1,29 +1,51 @@
 package SER315_GroupProject;
 
+import java.util.ArrayList;
+
 //Class that holds the information for a racer's license.
 public class License {
-	
-	//License attributes
-	private String purchaseDate;
-	private String expiryDate;
-	private String ownerName;
-	private CAT currentCAT;
-	
-	//License constructor
-	public License(String purchaseDate, String expiryDate, String ownerName, CAT currentCAT) {
-		this.purchaseDate = purchaseDate;
-		this.expiryDate = expiryDate;
-		this.ownerName = ownerName;
-		this.currentCAT = currentCAT;
-	}
-	
-	//Getters and setters
-	public String getPurchaseDate() {return purchaseDate;}
-	public String getExpiryDate() {return expiryDate;}
-	public String getOwnerName() {return ownerName;}
-	public CAT getCurrentCAT() {return currentCAT;}
-}
 
+    private static ArrayList<License> allLicenses;
+
+    //License attributes
+    private String purchaseDate;
+    private String expiryDate;
+    private String ownerName;
+    private CAT currentCAT;
+
+    //License constructor
+    public License(String purchaseDate, String expiryDate, String ownerName, CAT currentCAT) {
+        this.purchaseDate = purchaseDate;
+        this.expiryDate = expiryDate;
+        this.ownerName = ownerName;
+        this.currentCAT = currentCAT;
+    }
+
+    public static int findLicense(String ownerName){
+        for(int i = 0; i < allLicenses.size(); i++){
+            if(allLicenses.get(i).getOwnerName().equalsIgnoreCase(ownerName)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //Adds a license to the arraylist
+    public static void addLicense(License license){
+        allLicenses.add(license);
+    }
+
+    //Removes a license from the arraylist
+    public static void removeLicense(int index){
+        allLicenses.remove(index);
+    }
+
+    //Getters and setters
+    public String getPurchaseDate() {return purchaseDate;}
+    public String getExpiryDate() {return expiryDate;}
+    public String getOwnerName() {return ownerName;}
+    public CAT getCurrentCAT() {return currentCAT;}
+}
 
 
 
